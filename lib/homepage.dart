a8ee97ae6225fab7e2a3b123dbd3e6812bbc076c
MACPU1998 <Mahdiehr.rahbar@gmail.com>
2024-03-27 12:47:54 +0330

    mario

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -63,7 +63,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
     return Scaffold(
       body: Column(
         children: [
